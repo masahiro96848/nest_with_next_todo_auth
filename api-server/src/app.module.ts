211f0e245e0544273fcd633712dc346a90b7f400
masahiro96848 <feetwcpic9436@gmail.com>
2023-02-23 23:33:10 +0900

    todoのContorollerを作成

--- api-server/src/app.module.ts
+++ api-server/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TodoService } from './todo/todo.service';
+import { TodoController } from './todo/todo.controller';
 
 @Module({
   imports: [],
+  controllers: [TodoController],
 })
 export class AppModule {}
