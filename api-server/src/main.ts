f3e8ad05b55943639a27cf47edbc6cf9ec012923
masahiro96848 <feetwcpic9436@gmail.com>
2023-02-23 23:16:25 +0900

    APIのURLを設定。CORS対応にコードを修正

--- api-server/src/main.ts
+++ api-server/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // APIのURLを全て「/api」から始まるようにする
+  app.setGlobalPrefix('api');
+  // CORS対応
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
