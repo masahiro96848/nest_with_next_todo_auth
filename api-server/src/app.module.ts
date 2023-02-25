2fe1295aac2b7b7ef103f15850c3ebd820d0cd26
masahiro96848 <feetwcpic9436@gmail.com>
2023-02-25 22:17:33 +0900

    authのservice、module、controllerの雛形を作成

--- api-server/src/app.module.ts
+++ api-server/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TodoService } from './todo/todo.service';
 import { TodoController } from './todo/todo.controller';
 import { TodoModule } from './todo/todo.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TodoModule],
+  imports: [TodoModule, AuthModule],
 })
 export class AppModule {}
